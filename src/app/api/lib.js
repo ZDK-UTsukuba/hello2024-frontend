54a1d92baa1f288bfe7169f7a37215208d218653
Crow314 <master@crow31415.net>
2024-03-06 06:17:24 +0900

    Revert "Merge pull request #3 from ZDK-UTsukuba/add/api"
    This reverts commit 5ebd53ad2be858736d79c9c50f08f3014104cef7, reversing
    changes made to aa49cd9c6e70a2275b2efc67d26bb628375d396f.

--- src/app/api/lib.js
+++
@@ -1,4 +0,0 @@
-import { FetchRequests, CardRequests, testRequests } from "./api";
-
-export const CardRequestsImpl = new CardRequests(new FetchRequests())
-export const testRequestsImpl = new testRequests()
