92f41d934039f28b95e866629a862ba6b1c8b42a
oageo <kig.osumiakari@gmail.com>
2024-04-01 00:30:58 +0900

    メタデータに共通する部分を`src/app/layout.js`に設定

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,11 +1,19 @@
 export const metadata = {
-  title: "筑波大学 新入生情報Web 2024",
-  description: "筑波大学 全学学類・専門学群・総合学域群代表者会議",
+  title: {
+    template: "%s - 筑波大学 新入生情報Web 2024",
+    default: "筑波大学 新入生情報Web 2024"
+  },
+  description: "筑波大学の学生宿舎に入居する新入生向けの情報ポータル",
+  publisher: "筑波大学 全学学類・専門学群・総合学域群代表者会議",
+  openGraph: {
+    type: "website",
+    locale: 'ja_JP',
+  }
 };
 
 const Layout = ({ children }) => {
   return (
-    <html lang="en">
+    <html lang="ja">
       <body>{children}</body>
     </html>
   );
