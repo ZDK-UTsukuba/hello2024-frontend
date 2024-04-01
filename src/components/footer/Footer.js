fa23cf6f1dcfe90790783bea5dc674bbefccda31
もにょっとしたぷにぷにのなにか <134484570+monyopuni@users.noreply.github.com>
2024-04-02 03:05:46 +0900

    Footer画像の追加

--- src/components/footer/Footer.js
+++ src/components/footer/Footer.js
@@ -1,22 +1,30 @@
 import "@/app/globals.css";
-import "@/components/footer/stylesheet.css";
+import "./stylesheet.css";
 import Link from "next/link";
+import Image from 'next/image';
 
 
 export const Footer = () => {
   return (
-    <footer>
+    <footer className="footer-all">
 
-      <p>
+      <p className="footer-list">
         <Link href={`/`}>トップページ</Link >
         &nbsp;
         <Link href={`/article-list/all`}>記事一覧</Link >
         &nbsp;
         <Link href={`/faq-list`}>よくある質問</Link >
       </p>
-      <p>
+
+      <div className="footer-logo">
+        <a href="https://www.stb.tsukuba.ac.jp/~zdk/">
+          <Image src="/zdk_logo_full.png" alt="全学学類・専門学群・総合学域群代表者会議 ロゴ画像" width={100} height={25} />
+        </a>
+      </div>
+
+      <div>
         &copy; 全学学類・専門学群・総合学域群代表者会議 情報処理推進特別委員会 2024
-      </p>
+      </div>
     </footer >
   );
 };
