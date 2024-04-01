eb66f2226e2a133f7c9bec29dcf107977ec338f4
oageo <kig.osumiakari@gmail.com>
2024-04-02 07:33:09 +0900

    メニューバーの本体部分について`position: stickey`を適用した

--- src/components/menu-bar/MenuBar.js
+++ src/components/menu-bar/MenuBar.js
@@ -6,31 +6,33 @@ export const MenuBar = () => {
   const categories = ["宿舎生活", "自転車"];
   return (
     <div className="menu-bar-container">
-      <h2 className="menu-title">メニュー</h2>
-      <ul className="menu-items">
-        <div className="menu-item">
-          <li>
-            <Link href={`/article-list/all`}>記事一覧</Link>
-          </li>
-          <ul className="menu-category">
-            {categories.map((category) => (
-              <li>
-                <Link href={`/article-list/${category}`}>{category}</Link>
-              </li>
-            ))}
-          </ul>
-        </div>
-        <div className="menu-item">
-          <li>
-            <Link href={`/faq-list`}>よくある質問</Link>
-          </li>
-        </div>
-        <div className="menu-item">
-          <li>
-            <Link href={``}>質問フォーム↗</Link>
-          </li>
-        </div>
-      </ul>
+      <div className="menu-item-wrapper">
+        <h2 className="menu-title">メニュー</h2>
+        <ul className="menu-items">
+          <div className="menu-item">
+            <li>
+              <Link href={`/article-list/all`}>記事一覧</Link>
+            </li>
+            <ul className="menu-category">
+              {categories.map((category) => (
+                <li>
+                  <Link href={`/article-list/${category}`}>{category}</Link>
+                </li>
+              ))}
+            </ul>
+          </div>
+          <div className="menu-item">
+            <li>
+              <Link href={`/faq-list`}>よくある質問</Link>
+            </li>
+          </div>
+          <div className="menu-item">
+            <li>
+              <Link href={``}>質問フォーム↗</Link>
+            </li>
+          </div>
+        </ul>
+      </div>
     </div>
   );
 };
