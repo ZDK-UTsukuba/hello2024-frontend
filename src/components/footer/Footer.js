dff66e7ec66e93ebbb9ad6d440314e519ec4f2f5
もにょっとしたぷにぷにのなにか <134484570+monyopuni@users.noreply.github.com>
2024-04-02 01:47:21 +0900

    Linkの追加

--- src/components/footer/Footer.js
+++ src/components/footer/Footer.js
@@ -1,7 +1,21 @@
 import "@/app/globals.css";
+import Link from "next/link";
+
 
 export const Footer = () => {
   return (
-    <footer>&copy; 全学学類・専門学群・総合学域群代表者会議 情報処理推進特別委員会 2024</footer>
+    <footer>
+
+      <p>
+        <Link href={`/`}>トップページ</Link >
+        &nbsp;
+        <Link href={`/article-list/all`}>記事一覧</Link >
+        &nbsp;
+        <Link href={`/faq-list`}>よくある質問</Link >
+      </p>
+      <p>
+        &copy; 全学学類・専門学群・総合学域群代表者会議 情報処理推進特別委員会 2024
+      </p>
+    </footer >
   );
 };
