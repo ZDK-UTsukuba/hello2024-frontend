840437298abc714cb9021c572cc7c52c2202904f
もにょっとしたぷにぷにのなにか <134484570+monyopuni@users.noreply.github.com>
2024-04-02 03:09:27 +0900

    コピーライト・全代会ロゴの修正

--- src/components/footer/Footer.js
+++ src/components/footer/Footer.js
@@ -18,12 +18,14 @@ export const Footer = () => {
 
       <div >
         <a href="https://www.stb.tsukuba.ac.jp/~zdk/">
-          <Image src="/zdk_logo_full.png" alt="全学学類・専門学群・総合学域群代表者会議 ロゴ画像" width={100} height={25} />
+          <Image src="/zdk_logo_full.png" alt="全学学類・専門学群・総合学域群代表者会議 ロゴ画像" width={200} height={50} />
         </a>
       </div>
 
       <div>
-        &copy; 全学学類・専門学群・総合学域群代表者会議 情報処理推進特別委員会 2024
+        &copy; 全学学類・専門学群・総合学域群代表者会議 2024
+        <br />
+        新入生歓迎特別委員会、生活環境委員会、情報処理推進特別委員会
       </div>
     </footer >
   );
