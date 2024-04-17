354698e2cf7fd9058c976c65ad87ec44faa631a6
takum1-me <134484570+monyopuni@users.noreply.github.com>
2024-04-18 01:39:52 +0900

    記事一覧をカテゴリ単位に分割

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -3,6 +3,7 @@ import "@/components/header/stylesheet.css";
 import Link from "next/link";
 
 export const Header = () => {
+  const categories = ["宿舎生活", "自転車"];
   return (
     <header>
       <h1 className="page-title">
@@ -11,9 +12,11 @@ export const Header = () => {
 
       <nav>
         <ul className="nav-items">
-          <li>
-            <Link href={`/article-list/all`}>記事一覧</Link>
-          </li>
+          {categories.map((category) => (
+            <li key={category}>
+              <Link href={`/article-list/${category}`}>記事：{category}</Link>
+            </li>
+          ))}
           <li>
             <Link href={`/faq-list`}>よくある質問</Link>
           </li>
