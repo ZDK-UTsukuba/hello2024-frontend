064b48114eb9cd1398fa78f6edb6778d04c55939
takum1-me <134484570+monyopuni@users.noreply.github.com>
2024-04-18 01:39:22 +0900

    サイドバーの内容をheaderに追加

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -8,6 +8,21 @@ export const Header = () => {
       <h1 className="page-title">
         <Link href={`/`}>新入生情報Web</Link>
       </h1>
+
+      <nav>
+        <ul className="nav-items">
+          <li>
+            <Link href={`/article-list/all`}>記事一覧</Link>
+          </li>
+          <li>
+            <Link href={`/faq-list`}>よくある質問</Link>
+          </li>
+          {/*<li>
+            <Link href={``}>質問フォーム↗</Link>
+          </li>
+          */}
+        </ul>
+      </nav>
     </header>
   );
 };
